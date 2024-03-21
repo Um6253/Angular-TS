0083fd3401f373f5d624e109fe7fb4041212f860
Um6253 <um6253@srmist.edu.in>
2024-03-21 16:48:04 +0530

    [MANIKANTA]:Creating Display Note ts

---
+++ src/app/Components/display-notes/display-notes.component.ts
@@ -0,0 +1,35 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { NoteService } from '../../Services/Noteservices/note.service';
+import { EditNotesComponent } from '../edit-notes/edit-notes.component';
+import { MatDialog } from '@angular/material/dialog';
+//import { EventEmitter } from 'stream';
+
+@Component({
+  selector: 'app-display-notes',
+  templateUrl: './display-notes.component.html',
+  styleUrl: './display-notes.component.scss'
+})
+export class DisplayNotesComponent {
+  constructor(public dialog:MatDialog){}//public dialog:MatDailog,private data:DataService
+   message:any
+   @Input()NotesList:any;
+   @Output() RefreshEdit=new EventEmitter<string>();
+   @Output() RefreshArchive=new EventEmitter<string>();
+
+  EditNoteDialogBox(Notes:any){
+   const dialogbox= this.dialog.open(EditNotesComponent,{ 
+    width: "40%",
+    height:'auto',
+    data:Notes
+    })
+    
+    dialogbox.afterClosed().subscribe ((result: any) =>{
+    console.log(result);
+    this.RefreshEdit.emit(result);
+    
+    })
+    
+  }
+ 
+}
